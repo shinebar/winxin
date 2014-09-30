8ef62a69ad6e905b0f92b5c427526f7ea75e7bf1
shinebar <wangjinshan-wang@163.com>
2014-09-30 21:50:28 +0800

    new class

--- FMR/src/main/java/com/wjs/action/NewsAction.java
+++ FMR/src/main/java/com/wjs/action/NewsAction.java
@@ -3,4 +3,9 @@ package com.wjs.action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class NewsAction extends ActionSupport {
+	public static void main(String[] args){
+		System.getProperties().list(System.out);
+//		System.out.println(System.getProperty("user.name"));
+//		System.out.println(System.getProperty("java.library.path"));
+	}
 }
